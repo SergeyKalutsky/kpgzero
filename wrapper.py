22775532b9fb29e2e35aab06e18e67562b9e49e5
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-03-30 15:49:28 +0300

    fixed more bugs for platform

--- wrapper.py
+++ wrapper.py
@@ -2234,160 +2234,301 @@ schedule_unique = clock.schedule_unique
 unschedule = clock.unschedule
 each_tick = clock.each_tick
 # ========================================= TESTING AREA ==============================================================
-  
-WIDTH = sx = 854
-HEIGHT = sy = 480
-TITLE = "Clooky Clunker"
+# Pong is a two-dimensional sports game that simulates table tennis.
+# The player controls an in-game paddle by moving it vertically across
+# the left side of the screen, and can compete against either a
+# computer-controlled opponent or another player controlling a second
+# paddle on the opposing side. Players use the paddles to hit a ball
+# back and forth. The aim is for each player to reach eleven points
+# before the opponent; points are earned when one fails to return
+# the ball to the other.
+import random
 
-score, totalscore, clunkers = 0, 0, 0
-nextgoal = 0
-tgoal = -100
-clunks = []
-tbuy, buytext = -100, ""
-t = 0
+WIDTH = 800
+HEIGHT = 600
+TITLE = 'pong'
 
-buttonrects = [Rect((50, 120 + 85 * j, 180, 70)) for j in range(4)]
-buttonnames = ["auto-clunker", "clunkutron",
-               "turbo enclunkulator", "clunx capacitor"]
-buttoncosts = [10, 400, 12000, 250000]
+# a color used to draw things
+MAIN_COLOR = 'yellow'
 
+# width and height of a player paddle
+PADDLE_WIDTH = 15
+PADDLE_HEIGHT = 100
 
-def on_key_down(key):
-    if key == keys.ESCAPE:
-        exit()
+# radius of the tennis ball
+TENNIS_BALL_RADIUS = 10
 
-
-def on_mouse_down(button, pos):
-    global score, totalscore, clunkers, tbuy, buytext
-    if button != 1:
-        return
-
-    x, y = pos
-    # Click on the central circle
-    if (x - sx / 2) ** 2 + (y - sy / 2) ** 2 < 100 ** 2:
-        score += 1
-        totalscore += 1
-        # Add a "clunk" indicator at a pseudorandom place near the center
-        ix = sx / 2 + 12345678910. / (1 + t) % 1 * 200 - 100
-        iy = sy / 2 + 45678910123. / (1 + t) % 1 * 200 - 100
-        clunks.append((t, ix, iy))
-
-    # Click on one of the buttons
-    for j in range(len(buttonrects)):
-        rect, cost = buttonrects[j], buttoncosts[j]
-        if rect.collidepoint(x, y) and score >= cost:
-            score -= cost
-            clunkers += 10 ** j
-            tbuy = t
-            buytext = "+%s clunk/s" % (10 ** j)
-            buttoncosts[j] += int(round(cost * 0.2))
+LEFT_PLAYER = "left"
+RIGHT_PLAYER = "right"
 
 
-def update(dt):
-    global t
-    global score, totalscore, goaltext, tgoal, nextgoal
-    t += dt
-    score += clunkers * dt
-    totalscore += clunkers * dt
+class Paddle(Rect):
+    """
+    Paddle represents one player on the screen.
+    It is drawn like a long rectangle and positioned either left or
+    right on the screen.
+    Two helper methods move the paddle up or down.
+    """
 
-    # Check for next achievement
-    if totalscore > 100 * (1 << nextgoal):
-        goaltext = "Achievement unlocked:\nCL%sKY!" % ("O" * (nextgoal + 2))
-        tgoal = t
-        nextgoal += 1
+    def __init__(self, start_x, start_y):
+        super().__init__(start_x, start_y, PADDLE_WIDTH, PADDLE_HEIGHT)
 
-    clunks[:] = [c for c in clunks if t - c[0] < 1]
+    def up(self):
+        if self.y - 5 > 40:
+            self.y -= 5
+
+    def down(self):
+        if self.y + self.height + 5 < HEIGHT - 40:
+            self.y += 5
+
+    def draw(self):
+        screen.draw.filled_rect(self, MAIN_COLOR)
+
+
+class TennisBall():
+    """
+    Represents a tennis ball on the screen
+    """
+
+    def __init__(self, start_pos, dt):
+        """
+        Initialize the tennis ball position and set the movement rate
+        """
+        self.x, self.y = start_pos
+        self.dx = self.dy = dt
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
+
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+
+    def draw(self):
+        screen.draw.filled_circle(self.pos, TENNIS_BALL_RADIUS, MAIN_COLOR)
+
+
+class Game():
+
+    def __init__(self, player):
+        self.active_player = player
+        self.score_left = 0
+        self.score_right = 0
+
+        self.in_progress = False
+        self.computer_acting = False
+
+        # position paddles in the middle of the screen
+        middle = HEIGHT/2 - PADDLE_HEIGHT/2
+        self.left_paddle = Paddle(20, middle)
+        self.right_paddle = Paddle(WIDTH-40, middle)
+
+        self.set_ball(self.ball_pos)
+
+    @property
+    def ball_pos(self):
+        if self.active_player == LEFT_PLAYER:
+            return (20 + PADDLE_WIDTH + 10, self.left_paddle.centery)
+        else:
+            return (WIDTH - 35 - PADDLE_WIDTH, self.right_paddle.centery)
+
+    def set_ball(self, pos):
+        # a ball is set on the paddle of last player that got a point
+        dt = 5 if self.active_player == LEFT_PLAYER else -5
+        self.tennis_ball = TennisBall(pos, dt)
+
+    def position_ball(self):
+        # used when the player moves tha paddle and
+        # game is not in progress
+        self.tennis_ball.x, self.tennis_ball.y = self.ball_pos
+
+    def score_for_left(self):
+        self.in_progress = False
+        self.active_player = LEFT_PLAYER
+        self.score_left += 1
+        self.set_ball(self.ball_pos)
+
+    def score_for_right(self):
+        self.in_progress = False
+        self.active_player = RIGHT_PLAYER
+        self.score_right += 1
+        self.set_ball(self.ball_pos)
+
+    def proceed(self):
+        self.tennis_ball.move()
+
+        # bounce from the walls
+        if self.tennis_ball.y <= 40:
+            self.tennis_ball.dy = -self.tennis_ball.dy
+
+        if self.tennis_ball.y >= HEIGHT - 40:
+            self.tennis_ball.dy = -self.tennis_ball.dy
+
+        # bounce from the paddles
+        if self.left_paddle.collidepoint(self.tennis_ball.pos):
+            self.tennis_ball.dx = -self.tennis_ball.dx
+
+        if self.right_paddle.collidepoint(self.tennis_ball.pos):
+            self.tennis_ball.dx = -self.tennis_ball.dx
+
+        # if we didn't bounce, then that is a score
+        if self.tennis_ball.x <= 0:
+            self.score_for_right()
+
+        if self.tennis_ball.x >= WIDTH:
+            self.score_for_left()
+
+        if self.score_left == 11 or self.score_right == 11:
+            self.in_progress = False
+
+    # computer movement
+    def computer_launch(self):
+        self.in_progress = True
+        self.computer_acting = False
+
+    def computer_stop_acting(self):
+        self.computer_acting = False
+
+    def computer_move(self):
+        # move towards the center of the screen when the ball is
+        # travelling toward the enemy
+        if self.tennis_ball.dx > 0:
+            target_y = HEIGHT / 2
+        else:
+            # when the ball is on other side of screen, just move
+            # in general direction
+            if self.tennis_ball.x > WIDTH / 2:
+                delta = int(WIDTH * 0.25)
+                if self.tennis_ball.dy < 0:
+                    target_y = self.tennis_ball.y - delta
+                else:
+                    target_y = self.tennis_ball.y + delta
+            else:
+                # the ball is on our side, move with it
+                rnd = random.randint(40, 200)
+                if self.tennis_ball.dy < 0:
+                    target_y = self.tennis_ball.y - rnd
+                else:
+                    target_y = self.tennis_ball.y + rnd
+
+        target_y = max(40, min(target_y, HEIGHT - 80))
+
+        distance = abs(self.left_paddle.y - target_y)
+        duration = min(1.0, distance / 600.0)
+
+        animate(self.left_paddle, y=target_y, duration=.50,
+            on_finished=self.computer_stop_acting)
+
+    def computer_move_randomly(self):
+        # move the paddle randomly during one second before launching the ball
+        target_y = random.randint(40, HEIGHT - PADDLE_HEIGHT - 80)
+        distance = abs(self.left_paddle.y - target_y)
+        duration = max(0.1, distance / 200.0)
+        self.computer_total_duration += duration
+
+        if self.computer_total_duration + duration < 1.0:
+            on_finished = self.computer_move_randomly
+        else:
+            on_finished = self.computer_launch
+
+        animate(self.left_paddle, y=target_y, duration=duration,
+            on_finished=on_finished)
+
+    def computer_act(self):
+        if self.in_progress:
+            # predict where the ball will move and move towards it
+            self.computer_move()
+        elif self.active_player == LEFT_PLAYER:
+            # move randomly for a bit, then shoot the ball out
+            if not self.computer_acting:
+                self.computer_acting = True
+
+                self.computer_total_duration = 0.0
+                self.computer_move_randomly()
+
+    def draw(self):
+        # slightly gray background
+        screen.fill((64, 64, 64))
+
+        # show the score for the left player
+        screen.draw.text(
+            'Computer: {}'.format(self.score_left),
+            color=MAIN_COLOR,
+            center=(WIDTH/4 - 20, 20),
+            fontsize=48
+        )
+
+        # show the score for the right player
+        screen.draw.text(
+            'Player: {}'.format(self.score_right),
+            color=MAIN_COLOR,
+            center=(WIDTH/2 + WIDTH/4 - 20, 20),
+            fontsize=48
+        )
+
+        # a dividing line
+        screen.draw.line(
+            (WIDTH/2, 40),
+            (WIDTH/2, HEIGHT-40),
+            color=MAIN_COLOR)
+
+        if self.score_left == 11:
+            screen.draw.text(
+                'COMPUTER WINS!!!',
+                color=MAIN_COLOR,
+                center=(WIDTH/2, HEIGHT/2),
+                fontsize=96
+            )
+        elif self.score_right == 11:
+            screen.draw.text(
+                'PLAYER WINS!!!',
+                color=MAIN_COLOR,
+                center=(WIDTH/2, HEIGHT/2),
+                fontsize=96
+            )
+        else:
+            self.left_paddle.draw()
+            self.right_paddle.draw()
+            self.tennis_ball.draw()
+
+
+# first player is chosen randomly
+# player = LEFT_PLAYER if random.randint(0, 1000) % 2 == 0 else RIGHT_PLAYER
+player = LEFT_PLAYER
+game = Game(player)
 
 
 def draw():
-    screen.fill((0, 30, 30))
+    game.draw()
 
-    # Draw the circle in the middle
-    screen.draw.filled_circle((sx // 2, sy // 2), 106, 'black')
-    screen.draw.filled_circle((sx // 2, sy // 2), 100, '#884400')
 
-    # Draw the buttons using screen.draw.textbox
-    for rect, name, cost in zip(buttonrects, buttonnames, buttoncosts):
-        screen.draw.filled_rect(rect, "#553300")
-        screen.draw.filled_rect(rect.inflate(-8, -8), "#332200")
-        text = u"%s: %d\u00A0clunks" % (name, cost)
-        color = "white" if cost <= score else "#666666"
-        box = rect.inflate(-16, -16)
-        screen.draw.textbox(
-            text, box,
-            lineheight=0.9,
-            color=color,
-            owidth=0.5
-        )
+def update(dt):
+    if keyboard.up:
+        game.right_paddle.up()
+    elif keyboard.down:
+        game.right_paddle.down()
 
-    # Draw the HUD
-    hudtext = "\n".join([
-        "time played: %d" % t,
-        "clunks: %d" % score,
-        "all-time clunks: %d" % totalscore,
-        "clunks per second: %d" % clunkers,
-    ])
-    screen.draw.text(hudtext,
-        right=sx - 10,
-        top=120,
-        fontsize=32,
-        color=(0, 200, 0),
-        scolor=(0, 50, 0),
-        shadow=(-1, 1),
-        lineheight=1.3
-    )
+    # move the computer controlled paddle
+    game.computer_act()
 
-    # Draw the title using a gradient
-    screen.draw.text(
-        "Clooky Clunker",
-        midtop=(sx / 2, 10),
-        fontsize=64,
-        owidth=1.2,
-        color="#884400",
-        gcolor="#442200"
-    )
+    # set the position of the ball to be in the middle of the paddle
+    if not game.in_progress:
+        game.position_ball()
 
-    # Draw "clunk" indicators
-    for it, ix, iy in clunks:
-        dt = t - it
-        pos = ix, iy - 60 * dt
-        screen.draw.text(
-            "clunk",
-            center=pos,
-            fontname=None,
-            fontsize=28,
-            alpha=1 - dt,
-            shadow=(1, 1)
-        )
+    if game.in_progress:
+        game.proceed()
 
-    # Draw purchase indicator
-    if t - tbuy < 1:
-        dt = t - tbuy
-        pos = sx / 2, sy / 2
-        fontsize = 32 * (1 + 60 * dt) ** 0.2
-        screen.draw.text(
-            buytext, pos,
-            anchor=(0.5, 0.9),
-            fontsize=fontsize,
-            alpha=1 - dt,
-            shadow=(1, 1)
-        )
 
-    # Draw achievement unlocked text (text is centered even though we specify
-    # bottom right).
-    if t - tgoal < 2:
-        alpha = min(2 - (t - tgoal), 1)
-        screen.draw.text(
-            goaltext,
-            fontsize=48,
-            bottom=sy - 20,
-            right=sx - 40,
-            color="#AAAAFF",
-            gcolor="#4444AA",
-            shadow=(1.5, 1.5),
-            alpha=alpha,
-            align="center"
-        )
+
+def on_key_down(key):
+    # pressing SPACE launches the ball
+    if key == keys.SPACE:
+        if game.score_left == 11 or game.score_right == 11:
+            game.score_left = game.score_right = 0
+
+        if not game.in_progress and game.active_player == RIGHT_PLAYER:
+            game.in_progress = True
 # ========================================== MAIN LOOP ==================================================================
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
